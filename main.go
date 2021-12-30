64c029183696b554d31c6351eebfeb9d6360ef15
BaconPeeks <80063529+BaconPeeks@users.noreply.github.com>
2021-12-30 00:06:34 -0500

    Added new feature to se if they wanna play again
    I moved most of the UI navigation to other functions and call them when the input equals their name, Therefore cleaning up the main Golang function.

--- main.go
+++ main.go
@@ -9,15 +9,35 @@ import (
 
 func main() {
 	fmt.Printf("Welcome to bacon's random number generator!\n")
-	time.Sleep(1 * time.Second)
+	var inputFirst string
+	fmt.Scanln(&inputFirst)
+	if inputFirst == "Number" {
+		numberGenerator()
+	} else if inputFirst == "Kill" {
+		killScript()
+	}
+}
+
+func numberGenerator() {
 	fmt.Println("Please enter a number and the generator will generate a random from 0 to your number:")
 	var random int
 	fmt.Scanln(&random)
 	fmt.Printf("Here is your random number:\n")
 	time.Sleep(1 * time.Second)
-	rand.Seed(time.Now().UnixNano()) 
+	rand.Seed(time.Now().UnixNano())
 	fmt.Println(rand.Intn(random))
 	time.Sleep(1 * time.Second)
+	fmt.Println("Would you like to play again?\n" + "If so please type Y or N to continue.")
+	var playAgain string
+	fmt.Scanln(&playAgain)
+	if playAgain == "Y" {
+		numberGenerator()
+	} else if playAgain == "N" {
+		main()
+	}
+}
+
+func killScript() {
 	var input string
 	fmt.Println("Would you like to exit the script?")
 	fmt.Scanln(&input)
